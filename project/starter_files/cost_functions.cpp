aac8459ce65c7419e4e163ff2bb4de87096ea510
brubakerjeff <brubaker.jeff@gmail.com>
2025-12-27 19:48:05 -0500

    Update cost_functions.cpp

--- project/starter_files/cost_functions.cpp
+++ project/starter_files/cost_functions.cpp
@@ -70,9 +70,8 @@ double collision_circles_cost_spiral(const std::vector<PathPoint>& spiral,
           // TODO-Distance from circles to obstacles/actor: How do you calculate
           // the distance between the center of each circle and the
           // obstacle/actor
-          double dist = std::sqrt(
-             std::pow((circle_center_x - actor_center_x), 2) +
-             std::pow((circle_center_y - actor_center_y), 2));  // <- Update
+          double dist = std::sqrt(std::pow(actor_center_x-circle_center_x, 2) + std::pow(actor_center_y-circle_center_y, 2));  // <- Update
+
 
           collision = (dist < (CIRCLE_RADII[c] + CIRCLE_RADII[c2]));
         }
